f5563d7c9673e5b06ea2eaee5bb2d5f312b6b164
alv67 <alv67.web@gmail.com>
2021-05-11 21:45:56 +0200

    Fine Tutorial

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,7 @@
 <header>
   <div class="container">
     <img src="/img/amelamela_logo.svg" alt="Pool Alv logo">
-    <div class="center">AMeLaMela</div>
+    <div class="center">AMeLaMela Polls</div>
   </div>
 </header>
 
@@ -25,7 +25,7 @@
     top: 60%;
     left: 50%;
     transform: translate(-50%, -50%);
-    font-size: 32px;
+    font-size: 24px;
     text-shadow: 0px 0px 8px #333;
 
   }
